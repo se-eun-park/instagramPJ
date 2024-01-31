f9dffcb765941f14e6e14a551fc0d7112312c45c
박세은 <seeunbag30@gmail.com>
2024-01-31 22:24:36 +0900

    Temp: 임시 저장

--- prob22/src/story.js
+++ prob22/src/story.js
@@ -3,8 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const data = [
         {
             "id": "test1",
-            "name": "사람1",
-            "profileImg" : "https://via.placeholder.com/30",
+            "name": "user_name_1",
+            "profileImg" : "./asset/testImg/test1.png",
             "postImg": [
                 "https://via.placeholder.com/150",
                 "https://via.placeholder.com/150",
@@ -16,8 +16,8 @@ document.addEventListener('DOMContentLoaded', () => {
         },
         {
             "id": "test2",
-            "name": "사람2",
-            "profileImg" : "https://via.placeholder.com/30",
+            "name": "user_name_2",
+            "profileImg" : "./asset/testImg/test2.png",
             "postImg": [
                 "https://via.placeholder.com/150",
                 "https://via.placeholder.com/150",
@@ -29,8 +29,8 @@ document.addEventListener('DOMContentLoaded', () => {
         },
         {
             "id": "test3",
-            "name": "사람3",
-            "profileImg" : "https://via.placeholder.com/30",
+            "name": "user_name_3",
+            "profileImg" : "./asset/testImg/test3.png",
             "postImg": [
                 "https://via.placeholder.com/150",
                 "https://via.placeholder.com/150",
@@ -42,24 +42,59 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     ]
 
-    const profileContainer = document.getElementById('profileContainer');
-    const userName = document.getElementById('userName')
+    const profileContainer = document.getElementsByClassName('profileContainer')[0];
+    const userName = document.getElementsByClassName('userName')[0];
+
+    // img가 불러와질 때 마다 간격을 유지하며 오도록 설정해주는 변수
+    let offset_img = 2.2;
+    const space_img =  6.125;
+
+    let offset_name = 1.6;
 
     data.forEach(user => {
         const img = document.createElement('img');
-        const name = document.createElement('div');
+        // 이미지를 감싸는 요소들
+        const imgBack = document.createElement('div');
+        const imgStroke = document.createElement('div');
+
+        const nameSpan = document.createElement('span');
 
         img.src = user.profileImg;
         img.alt = `${user.name}의 프로필 사진`;
         img.style.cursor = 'pointer';
+        img.style.left = `${offset_img}rem` 
         img.addEventListener('click', () => {
             user.isStoryViewed = true;
             console.log(`${user.name}의 스토리 방문`, user.isStoryViewed);
-        });
-        profileContainer.appendChild(img);
 
-        name.textContent = user.name;
-        userName.appendChild(name);
+            // 스토리 방문 시, style 변경
+            if (user.isStoryViewed) {
+                imgStroke.style.width = `4.125rem`; // 기존 width에서 0.1rem 감소
+                imgStroke.style.height = `4.125rem`; // 기존 height에서 0.1rem 감소
+                imgStroke.style.background = '#000'; // 색상을 검은색으로 변경
+            }
+        });
+
+        
+        
+        imgBack.classList.add("imgBack");
+        imgStroke.classList.add("imgStroke");
+
+        imgBack.style.left = `${offset_img}rem`
+        imgStroke.style.left = `${offset_img}rem` 
+
+        offset_img += imgStroke.offsetWidth + space_img;
+        
+        profileContainer.appendChild(img);
+        profileContainer.appendChild(imgBack);
+        profileContainer.appendChild(imgStroke);
+        
+        nameSpan.textContent = user.name;
+        nameSpan.classList.add("name");
+
+        nameSpan.style.marginRight = `${offset_name}rem`
+
+        userName.appendChild(nameSpan);
 
     });
 
